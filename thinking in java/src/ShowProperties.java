7fdd010de16107e12b67e3f1c37c3c25262a15bd
HEKEH <hekai02@163.com>
2019-07-31 10:59:21 +0800

    branch

--- thinking in java/src/ShowProperties.java
+++ thinking in java/src/ShowProperties.java
@@ -8,5 +8,6 @@ public class ShowProperties {
 		System.getProperties().list(System.out);
 		System.out.println(System.getProperty("1user.name"));
 		System.out.println(System.getProperty("java.library.path"));
+		System.out.println("branch");
 	}
 }
